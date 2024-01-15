6a1931a84ea349f61556c719a72d73497053a79b
BelenRoman <124020263+BelenRomanCervantes@users.noreply.github.com>
2024-01-15 15:42:56 -0600

    Creacion de una lista de usuarios y su respectivo html llamado list

--- PruebaSpring/springboot-web/src/main/java/com/belen/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ PruebaSpring/springboot-web/src/main/java/com/belen/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.belen.curso.springboot.webapp.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.*;
 
 import com.belen.curso.springboot.webapp.springbootweb.models.User;
 
@@ -13,9 +15,22 @@ public class UserController {
     public String details(Model model) {
 
         User user = new User("Belen", "Roman");
+        user.setEmail("belen@correo.com");
         model.addAttribute("title", "Hola Mundo Spring Boot");
         model.addAttribute("user", user);
         return "details";
 
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = Arrays.asList(new User("Pablo","Herrera"), 
+        new User("Viridiana", "Sanchez", "viri@correo.com"), 
+        new User("Aldo", "Perez", "aldo@correo.com"),
+        new User("Andres", "Doe"));
+
+        model.addAttribute("users", users);
+        model.addAttribute("title","Listado de usuarios!");
+        return "list";
     }
 }
--- PruebaSpring/springboot-web/src/main/java/com/belen/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ PruebaSpring/springboot-web/src/main/java/com/belen/curso/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.belen.curso.springboot.webapp.springbootweb.controllers;
 
+
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.belen.curso.springboot.webapp.springbootweb.models.User;
 import com.belen.curso.springboot.webapp.springbootweb.models.dto.UserDto;
+
+
 
 @RestController
 @RequestMapping("/api")
@@ -24,6 +29,26 @@ public class UserRestController {
 
         return userDto;
     }
+
+    @GetMapping("/list")   
+    public List<User> list() {
+        User user = new User("Belen", "Roman");
+        User user2 = new User("Pablo", "Herrera");
+        User user3 = new User("Viridiana", "Sanchez");
+        
+
+        List<User> users = Arrays.asList(user,user2,user3);
+        /*
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user2);
+        users.add(user3);
+        */
+
+        return users;
+    }
+
+
         @GetMapping("/details-map")
         public Map<String, Object> detailsMap() {
             User user = new User("Belen", "Roman");
